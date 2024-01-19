aa840716b61dea9d9fc766c97aa96a88ff60ed8b
GloRosado07/wdd230 <address>
2024-01-18 18:46:57 -0600

    adding lesson1 assignment

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send("Joe");
+  res.send("Stella");
 });
 
-
+const port = 3000;
  
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+app.listen(process.env.PORT || port, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || port));
 });
